367199c6d72bdd96319932e140ab985877d7e292
Arthur <arthur@we7.com>
2014-10-30 14:26:29 +0000

    removed cron code and commented out broken git call for some reason git soes not work on startup.

--- code/updater.py
+++ code/updater.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python
 import subprocess
 
-#print "Setting Cron"
-#fo = open("/etc/cron.d/wezbot", "wb")
-#fo.write("@reboot python /home/pi/wezbot/code/updater.py > /home/pi/wezbot/code/updater.log");
-#fo.close()
-
-
-print "Updating"
-command = ["git", "fetch", "origin"]
-subprocess.check_output(command)
-command = ["git", "pull", "origin", "master"]
-subprocess.check_output(command)
+#print "Updating"
+#command = ["git", "fetch", "origin"]
+#subprocess.check_output(command)
+#command = ["git", "pull", "origin", "master"]
+#subprocess.check_output(command)
 #pull from git
 
 print "Starting wezbot"
