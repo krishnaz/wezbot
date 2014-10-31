dec39edd89f38d88660e57f7120f7f536d3773a7
Arthur <arthur@we7.com>
2014-10-31 13:00:19 +0000

    stepper test

--- code/wezbot.py
+++ code/wezbot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 from time import sleep
 import pifacedigitalio as p
 
@@ -10,19 +11,11 @@ ID = 1
 if __name__ == "__main__":
     p.init()
     while True:
-        if (p.digital_read(0)):
-                LOOPCOUNT = max(LOOPCOUNT / 2, 0.05)
-        if (p.digital_read(1)):
-                LOOPCOUNT = 100
-        if (p.digital_read(2)):
-                LOOPCOUNT = LOOPCOUNT * 2
 
-        COUNT -= 1
-        if (COUNT <= 0):
-                COUNT = LOOPCOUNT
-                p.digital_write(ID, 0)
-                ID+=1
-                ID=ID % 8
-                p.digital_write(ID, 1)
-        sleep(0.01)
+        p.digital_write(ID, 1)
+        ID += 1
+        ID = ID % 4
+        p.digital_write(ID, 0)
+
+        sleep(0.0001)
 
